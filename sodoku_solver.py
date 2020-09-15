f358a4e2bafdb0f0b1842576938855342286a92f
Blerkk <31781383+Blerkk@users.noreply.github.com>
2020-09-15 16:06:05 +0200

    meno

---
+++ sodoku_solver.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+grid = [[5,3,0,0,7,0,0,0,0],
+        [6,0,0,1,9,5,0,0,0],
+        [0,9,8,0,0,0,0,6,0],
+        [8,0,0,0,6,0,0,0,3],
+        [4,0,0,8,0,3,0,0,1],
+        [7,0,0,0,2,0,0,0,6],
+        [0,6,0,0,0,0,2,8,0],
+        [0,0,0,4,1,9,0,0,5],
+        [0,0,0,0,8,0,0,7,9]]
+
+def lehetseges(x,y,n):
+    global grid
+    for i in range(0,9):
+        if grid[y][i] == n:
+            return False
+    for i in range(0,9):
+        if grid[i][x] == n:
+            return False
+    xVar = (x//3)*3
+    yVar = (y//3)*3
+    for i in range(0,3):
+        for j in range(0,3):
+            if grid[yVar+i][xVar+i] == n:
+                return False
+    return True
+
+def megoldo():
+    global grid
+    for y in range(9):
+        for x in range(9):
+            if grid[y][x] == 0:
+                for n in range(1,10):
+                    if lehetseges(x,y,n):
+                        grid[y][x] = n
+                        megoldo()
+                        grid[y][x] = 0
+                return
+    print(np.matrix(grid))
+    input("Tovabb?")
+
+megoldo()
